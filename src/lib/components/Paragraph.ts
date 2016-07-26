b5bb2fb976e32748a3a04c2a818a4ee50f2e083c
grzhan <envy518@gmail.com>
2016-07-26 17:15:26 +0800

    feature(component): create component 'Paragraph'

---
+++ src/lib/components/Paragraph.ts
@@ -0,0 +1,28 @@
+export class Paragraph {
+    public startLine;
+    public endLine;
+    public startOffset;
+    public endOffset;
+    public startPos;
+    public endPos;
+    private context;
+
+    constructor(context, startLine, startOffset, endLine, endOffset) {
+        this.context = context;
+        this.startLine = startLine;
+        this.endLine = endLine;
+        this.startOffset = startOffset;
+        this.endOffset = endOffset;
+        this.startPos = this.calcPos(startLine, startOffset);
+        this.endPos = this.calcPos(endLine, endOffset);
+    }
+
+    public calcPos(lineNo, offset) {
+        let pos = 0;
+        for (let i=0; i<lineNo-1; i++) {
+            pos += this.context.lines['raw'][i].length;
+        }
+        pos += offset;
+        return pos;
+    }
+}
