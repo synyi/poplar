e36acfb7df280590fa2056b37f1de2fc02fd15e0
longfangsong <longfangsong@icloud.com>
2018-07-09 14:31:20 +0800

    :hammer_and_wrench: 多层标注

--- src/test/TestDataSource.ts
+++ src/test/TestDataSource.ts
@@ -4,11 +4,12 @@ import {Label} from "../annotator/Store/Label";
 
 export class TestDataSource implements AnnotatorDataSource {
     labels = [
-        // new Label('测试', 0, 5),
-        // new Label('测试', 6, 7),
-        // new Label('测试测试', 10, 14),
-        // new Label('测试测试', 19, 23),
-        // new Label('测试测试', 81, 85)
+        new Label('测试', 0, 5),
+        new Label('测试', 6, 7),
+        new Label('测试测试', 10, 14),
+        new Label('测试测试', 19, 23),
+        new Label('测试测试', 81, 85),
+        new Label('测试', 82, 84)
     ];
 
     addConnection(connection: Connection) {
@@ -20,7 +21,7 @@ export class TestDataSource implements AnnotatorDataSource {
 
     getRawContent(): string {
         // 实际计算时，不计算空白字符
-        //      0 123       4 567 8 901   2 34  5 678   9 012   3 456
+        //      0 1    2 3 45 6 78 90 1 23 4 56 7 89 01 2 34 56
         return "测试。\n测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试。" +
             "测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测测试测试测试测试测试。\n" +
             "测试。\n" +
